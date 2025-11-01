046feda5e8da1ab9f86258667f2500b023be4812
Jordi Sayerza <jsayerza.comptes@gmail.com>
2025-11-01 18:07:57 +0100

    ex00: OK

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -11,8 +11,12 @@
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-#include "ScalarConverterFuncs.hpp"
 #include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <cfloat>
+#include <cstdlib>
+#include <climits>
 
 ScalarConverter::ScalarConverter()
 {
@@ -39,39 +43,189 @@ ScalarConverter::~ScalarConverter()
 }
 
 
-//// Functions
+void printChar (int c, bool isPossible = true)
+{
+	if (!isPossible || c < 0 || c > 127)
+		std::cout << "char:   impossible" << std::endl;
+	else if (c < 32 || c == 127)
+		std::cout << "char:   Non displayable" << std::endl;
+	else
+		std::cout << "char:   " << static_cast<char>(c) << std::endl;
+}
+
 void printInt (int i, bool isPossible = true)
 {
 	if (!isPossible)
-		std::cout << "int: impossible" << std::endl;
+		std::cout << "int:    impossible" << std::endl;
 	else
-		std::cout << "int: '" << i << "'" << std::endl;
+		std::cout << "int:    " << i << std::endl;
 }
 
-void printChar (char c, bool isPossible = true)
+void	printFloat(float f, bool isPossible = true)
 {
+	// std::cout << "   printFloat - f: " << f << std::endl;
 	if (!isPossible)
-		std::cout << "char: impossible" << std::endl;
-	else if (c < 32 || c > 126)
-		std::cout << "char: Non displayable" << std::endl;
+		std::cout << "float:  impossible" << std::endl;
+	else if (std::isinf(f) || f > FLT_MAX || f < -FLT_MAX)
+	{
+		if (f > 0)
+    		std::cout << "float:  +inff" << std::endl;
+		else
+    		std::cout << "float:  -inff" << std::endl;
+	}
+	else if (std::isnan(f))
+    	std::cout << "float:  nanf" << std::endl;
 	else
-		std::cout << "char: '" << c << "'" << std::endl;
+	{
+		float absF = std::abs(f);
+		if (absF > 1e7 || (absF > 0 && absF < 1e-6))
+			std::cout << "float:  " << std::scientific 
+                      << std::setprecision(1) << f << "f" << std::endl;	
+		else
+			std::cout << "float:  " << std::setprecision(1) << std::fixed << f << "f" << std::endl;
+	}
 }
 
-void	printFloat(float f, bool isPossible = true) {
-	if (!isPossible)
-		std::cout << "float: impossible" << std::endl;
-	else
-		std::cout << "float: " << std::setprecision(1) << std::fixed << f << "f" << std::endl;
-}
-
-void	printDouble(double d, bool isPossible = true) {
+void	printDouble(double d, bool isPossible = true)
+{
+	// std::cout << "   printDouble - d: " << d << std::endl;
 	if (!isPossible)
 		std::cout << "double: impossible" << std::endl;
+	else if (std::isinf(d))
+		{
+		if (d > 0)
+			std::cout << "double: +inf" << std::endl;
+		else
+			std::cout << "double: -inf" << std::endl;
+		}
+	else if (std::isnan(d))
+		std::cout << "double: nan" << std::endl;
+	else if (std::abs(d) > 1e15)
+		std::cout << "double: " << d << std::endl;
 	else
 		std::cout << "double: " << std::setprecision(1) << std::fixed << d << std::endl;
 }
 
+
+////Convert to functions
+void	toPseudoLiteral(const std::string& str)
+{
+	printChar('c', false);
+	printInt(0, false);
+	if (str == "nanf" || str == "-inff" || str == "+inff")
+	{
+		std::cout << "float:  " << str << std::endl;
+		std::cout << "double: " << str.substr(0, str.length() - 1) << std::endl;
+	}
+	else
+	{
+		std::cout << "float:  " << str << "f" << std::endl;
+		std::cout << "double: " << str << std::endl;
+	}
+}
+
+void	toChar(const std::string& str)
+{
+	char c = str[0];
+	printChar(c);
+	printInt(static_cast<int>(c));
+	printFloat(static_cast<float>(c));
+	printDouble(static_cast<double>(c));
+}
+
+float	toFloat(std::string str)
+{
+	if (!str.empty() && str[str.length() -1] == 'f')
+		str.erase(str.length() - 1);
+
+	// strtod controla overflow/inf, posa inf, si aplica
+	char* end;
+    double d = std::strtod(str.c_str(), &end);
+	bool validConversion = (end != str.c_str() && *end == '\0');
+
+	float f = static_cast<float>(d);
+
+	std::stringstream ss1;
+	int i;
+	ss1 << str;
+	ss1 >> i;
+
+	printChar(static_cast<int>(f));
+	if (ss1.fail() || d < INT_MIN || d > INT_MAX)
+		printInt(0, false);
+	else
+		printInt(static_cast<int>(d));	//// Uso double per a millor rang
+	if (!validConversion)
+		printFloat(0, false);
+	else
+		printFloat(f);
+	printDouble(d);
+
+	ss1.clear();
+
+	return (f);
+}
+
+void	toDouble(const std::string& str)
+{
+	//// strtod controla overflow/inf, posa inf, si aplica
+	char* end;
+    double d = std::strtod(str.c_str(), &end);
+	bool validDouble = (end != str.c_str() && (*end == '\0' || *end == 'f'));
+
+	int i;
+	std::stringstream ss1;
+	ss1 << str;
+	ss1 >> i;
+
+	printChar(static_cast<int>(d));
+	if (ss1.fail() || d < INT_MIN || d > INT_MAX)
+		printInt(0, false);
+	else
+		printInt(static_cast<int>(d));
+	if (d < -FLT_MAX || d > FLT_MAX)
+		printFloat(0, false);
+	else
+		printFloat(static_cast<float>(d));
+	if (!validDouble)
+		printDouble(0, false);
+	else
+		printDouble(d);
+	
+	ss1.clear();
+}
+
+void	toInt(const std::string& str)
+{
+	// std::cout << "   toInt - str: " << str << std::endl;
+	//// strtod controla overflow/inf, posa inf, si aplica
+	char* end;
+    double d = std::strtod(str.c_str(), &end);
+	bool validConversion = (end != str.c_str() && *end == '\0');
+
+	float f = static_cast<float>(d);
+
+	std::stringstream ss1;
+	int i;
+	ss1 << str;
+	ss1 >> i;
+
+	printChar(static_cast<int>(i));
+	if (ss1.fail())
+		printInt(0, false);
+	else
+		printInt(static_cast<int>(i));
+	printFloat(f);
+	if (!validConversion)
+		printDouble(0, false);
+	else
+		printDouble(d);
+	
+	ss1.clear();
+}
+
+
+////Check functions
 bool	isPseudoLiteral(const std::string& str)
 {
 	std::string pseudoLiterals[6] = {"nan", "-inf", "+inf", "nanf", "-inff", "+inff"};
@@ -81,22 +235,6 @@ bool	isPseudoLiteral(const std::string& str)
 			return (true);
 	}
 	return (false);
-}
-
-void	toPseudoLiteral(const std::string& str)
-{
-	printChar('c', false);
-	printInt(0, false);
-	if (str == "nanf" || str == "-inff" || str == "+inff")
-	{
-		std::cout << "float: " << str << std::endl;
-		std::cout << "double: " << str.substr(0, str.length() - 1) << std::endl;
-	}
-	else
-	{
-		std::cout << "float: " << str << "f" << std::endl;
-		std::cout << "double: " << str << std::endl;
-	}
 }
 
 bool	isValid(const std::string& str) 
@@ -127,9 +265,10 @@ bool	isValid(const std::string& str)
 	return (true);
 }
 
+
+////Convert main function
 void ScalarConverter::convert(const std::string& str)
 {
-	// std::cout << "str: " << str << std::endl;
 	if (!isValid(str))
 	{
 		printChar('c', false);
@@ -139,9 +278,16 @@ void ScalarConverter::convert(const std::string& str)
 		return;
 	}
 	if (isPseudoLiteral(str))
+	{
 		toPseudoLiteral(str);
+		return;
+	}
+	if (str.length() == 1 && !isdigit(str[0]))
+		toChar(str);
+	else if (!str.empty() && str[str.length() -1] == 'f')
+		toFloat(str);
+	else if (str.find('.') != std::string::npos)
+		toDouble(str);
 	else
-		std::cout << "altres" << std::endl;
+		toInt(str);
 }
-
-
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,68 +15,58 @@
 #include "ScalarConverter.hpp"
 
 void	test_converter(std::string str) {
-	std::cout << "--- Input: " << str << " ---" << std::endl;
+	std::cout << "--- Input: " << str << std::endl;
 	ScalarConverter::convert(str);
 	std::cout << std::endl;
 }
 
 int	main(int argc, char** argv)
 {
-	// if (argc != 2)
-	// {
-	// 	std::cerr << "Error: Usage: ./convert {param}" << std::endl;
-	// 	return (1);
-	// }
+	if (argc != 2)
+	{
+		std::cerr << "Error: Usage: ./convert {param}" << std::endl;
+		return (1);
+	}
 
-	// std::string input(argv[1]);
-	// ScalarConverter::convert(input);
-	// std::cerr << "argv[1]: " << argv[1] << std::endl;
-	// ScalarConverter::convert(argv[1]);
+	std::string input(argv[1]);
+	ScalarConverter::convert(input);
 
-	(void)argc;
-	(void)argv;
-	test_converter("0");
-	test_converter("42");
-	test_converter("42.0");
-	test_converter("42.0f");
-	test_converter("42.2");
-	test_converter("-42.2");
-	test_converter("42.2f");
-	test_converter("42...2f");
-	test_converter("-inff");
-	test_converter("+inf");
-	test_converter("nan");
-	test_converter("invalid");
-	test_converter("42.2ff");
-	test_converter("4.2.2f");
-	test_converter("--42");
+	// (void)argc;
+	// (void)argv;
+	// test_converter("0");
+	// test_converter("42");
+	// test_converter("42.0");
+	// test_converter("42.2");
+	// test_converter("-42.2");
+	// test_converter("42.0f");
+	// test_converter("42.2f");
+	// test_converter("-42.2f");
+	// test_converter("14.0f");
+	// test_converter("14.2f");
+	// test_converter("-14.2f");
+	// test_converter("142.0f");
+	// test_converter("142.2f");
+	// test_converter("-142.2f");
+	// test_converter("42...2f");
+	// test_converter("42.2ff");
+	// test_converter("4.2.2f");
+	// test_converter("-inff");
+	// test_converter("+inf");
+	// test_converter("nan");
+	// test_converter("invalid");
+	// test_converter("--42");
+	// test_converter("a");
+	// test_converter("abc");
+	// test_converter("");
+	// test_converter("-1");
+	// test_converter("2147483647");
+		//La conversión 2147483647 → 2147483648.0f es el comportament estàndar de C++ degut a les limitacions de precisió de float. No és un bug, és una característica del tipus de dada.
+		//Els floats només tenen ~7 dígits significatius i aquest arrodoniment és inevitable per a números tan grans.	
+	// test_converter("2147483648");
+	// test_converter("999999999999999999");
+	// test_converter("-999999999999999999");
+	// test_converter("999999999999999999f");
+	// test_converter("999999999999999999999999999999999999999999.9");
 
 	return (0);
 }
-
-/*
-	test_converter("0");
-	test_converter("42");
-	test_converter("42.0");
-	test_converter("42.0f");
-	test_converter("42.2");
-	test_converter("-42.2");
-	test_converter("42.2f");
-	test_converter("-inff");
-	test_converter("+inf");
-	test_converter("nan");
-	test_converter("invalid");
-	test_converter("42.2ff");
-	test_converter("4.2.2f");
-	test_converter("--42");
-	test_converter("2147483647");
-	test_converter("2147483648");
-	test_converter("");
-	test_converter("999999999999999999f");
-	test_converter("999999999999999999999999999999999999999999.9");
-	test_converter("-1");
-*/
-/*
-https://github.com/mcombeau/Cpp_Modules/tree/main/cpp06
-https://github.com/sylvainnicolet-42/42-CPP-Module-06/tree/master
-*/
